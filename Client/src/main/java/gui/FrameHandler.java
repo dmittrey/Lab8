81cd846f4dee48052ee8e17837a223fa7c3a0aba
dmittrey <dmitryzubakhin@mail.ru>
2021-10-08 23:55:34 +0300

    logging and register

--- Client/src/main/java/gui/FrameHandler.java
+++ Client/src/main/java/gui/FrameHandler.java
@@ -1,11 +1,21 @@
 package gui;
 
+import utility.*;
+
+import javax.swing.*;
+import java.util.Arrays;
+import java.util.logging.Logger;
+
 public class FrameHandler {
 
-    private AddDetailsModel addDetailsModel;
-    private LoginModel loginModel;
-    private RegisterModel registerModel;
-    private MainModel mainModel;
+    private static final Logger logger = Logger.getLogger(CommandManager.class.getName());
+
+    private final CommandReader commandReader;
+    private final AddDetailsModel addDetailsModel;
+    private final LoginModel loginModel;
+    private final RegisterModel registerModel;
+    private final MainModel1 mainModel;
+    private final JFrame jFrame;
     private static FrameHandler instance;
 
     public static FrameHandler getInstance() {
@@ -14,26 +24,48 @@ public class FrameHandler {
     }
 
     private FrameHandler() {
-    }
-
-    public void initial(){
-//        addDetailsModel = new AddDetailsModel();
+        commandReader = CommandReader.getInstance();
+        addDetailsModel = new AddDetailsModel();
         loginModel = new LoginModel();
         registerModel = new RegisterModel();
-        mainModel = new MainModel();
+        mainModel = new MainModel1();
+        jFrame = new MainFrame();
     }
 
     public void start(){
-        loginModel.setVisible(true);
+        loginModel.setPanel(jFrame);
+        jFrame.setVisible(true);
     }
 
-    public void swapAuth(){
-        loginModel.setVisible(false);
-        registerModel.setVisible(true);
+    public void setAuth(){
+        loginModel.setPanel(jFrame);
     }
 
-    public void swapRegister(){
-        registerModel.setVisible(false);
-        loginModel.setVisible(true);
+    public void setRegister(){
+        registerModel.setPanel(jFrame);
+    }
+
+    public void setMain(){
+        mainModel.setPanel(jFrame);
+    }
+
+    public void login(String anUsername, char[] aPassword) {
+        Session session = new Session(anUsername, new String(aPassword), TypeOfSession.Login);
+        TypeOfAnswer status = commandReader.execute(session);
+        if ((commandReader.execute(session) != TypeOfAnswer.SUCCESSFUL)) {
+            loginModel.setWarn(status);
+        } else {
+            setMain();
+        }
+    }
+
+    public void register(String anUsername, char[] aPassword){
+        Session session = new Session(anUsername, new String(aPassword), TypeOfSession.Register);
+        TypeOfAnswer status = commandReader.execute(session);
+        if ((commandReader.execute(session) != TypeOfAnswer.SUCCESSFUL)) {
+            registerModel.setWarn(status);
+        } else {
+            setMain();
+        }
     }
 }
